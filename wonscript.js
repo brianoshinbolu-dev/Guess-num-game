abb999297515d981a5109354dabe6c2f2da05077
Brian Oshinbolu <105751295+brianoshinbolu-dev@users.noreply.github.com>
2022-07-22 10:51:01 +0100

    Add files via upload

---
+++ wonscript.js
@@ -0,0 +1,18 @@
+function tryagain(){
+    location.assign("index.html");
+}
+
+// function WriteToFile(passForm) { 
+ 
+//     set fso = CreateObject("Scripting.FileSystemObject");  
+//     set s   = fso.CreateTextFile("filename.txt", True); 
+ 
+//     var firstName = document.getElementById('Name'); 
+//     var lastName  = document.getElementById('Whatsapp'); 
+ 
+//     s.writeline("First Name :" + FirstName); 
+//     s.writeline("Last Name :" + lastName); 
+ 
+//     s.writeline("-----------------------------"); 
+//     s.Close(); 
+//  } 
